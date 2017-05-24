7c0fe863f7b6be809848e3fb41c7b17972782e20
jy <yiuj@usc.edu>
2017-05-24 16:04:52 -0700

    Adds one button now

--- button.js
+++ button.js
@@ -2,11 +2,12 @@
  * You must include the dependency on 'ngMaterial' 
  */
 var tasksP = document.getElementById("tasks");
+var btn = document.getElementById("addButton");
 var text = tasksP.innerHTML;
 var tasks = []
 var app = angular.module('BlankApp', ['ngMaterial']);
 
-app.controller('BlankCtrl', function ($scope, $mdDialog) {
+app.controller('BlankCtrl', function ($scope, $mdDialog, $compile) {
   $scope.showConfirm = function (ev) {
     $mdDialog.show(
       $mdDialog.alert()
@@ -23,8 +24,9 @@ app.controller('BlankCtrl', function ($scope, $mdDialog) {
 
   $scope.tasks = [];
   $scope.addTask = function () {
-    window.alert("Tasks added");
-    tasksP.innerHTML = tasksP.innerHTML + tasksP.innerHTML;
+    //window.alert("Tasks added");
+    angular.element(document.body).append($compile("<md-button class='md-raised md-primary md-hue-1' ng-click='showConfirm($event)'>Task</md-button>")($scope));
+    //tasksP.innerHTML = tasksP.innerHTML + "<br><md-button class='md-raised md-primary md-hue-1' ng-click='addTask()'>Task</md-button><br>";
 
   };
 
